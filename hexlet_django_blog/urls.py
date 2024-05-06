4bf413f43d4a94963fc8b1eb39185138c106329c
avolosevich <Alexander.Volosevich@nexign.com>
2024-05-06 12:46:04 +0300

    forgotten slash

--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -22,6 +22,6 @@ from hexlet_django_blog import views
 
 urlpatterns = [
     path("", views.index),
-    path("about", views.about),
+    path("about/", views.about),
     path("admin/", admin.site.urls),
 ]
