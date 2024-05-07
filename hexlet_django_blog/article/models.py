a1cf627c9db34199baa268eaadbb2592280f0209
avolosevich <Alexander.Volosevich@nexign.com>
2024-05-07 15:53:18 +0300

    use django ORM and ignore migrations

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
+class Article(models.Model):
+    name = models.CharField(max_length=200)
+    body = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 # Create your models here.
