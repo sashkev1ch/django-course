f750c74b96848708cfec77506bb4febfd9ec582c
Alexander Volosevich <sashkevich23@gmail.com>
2024-05-20 14:12:04 +0200

    use form.CharField for validation rules

--- hexlet_django_blog/article/forms.py
+++ hexlet_django_blog/article/forms.py
@@ -1,13 +1,18 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, CharField
 from hexlet_django_blog.article.models import ArticleComment, Article
 
 class CommentArticleForm(ModelForm):
+    content = CharField(max_length=100)
+
     class Meta:
         model = ArticleComment
         fields = ["content"]
 
 
 class CreateArticleForm(ModelForm):
+    name = CharField(max_length=200)
+    body = CharField(max_length=1000)
+
     class Meta:
         model = Article
         fields = ["name", "body"]
